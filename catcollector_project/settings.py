931488fdfac583c48bcf49bd3c279cc0b07f08ab
jpardike2 <jpardike@gmail.com>
2020-10-30 09:39:49 -0700

    Adds django on heroku config

--- catcollector_project/settings.py
+++ catcollector_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_on_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -122,3 +123,6 @@ STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = '/cats/index/'
 LOGOUT_REDIRECT_URL = '/'
+
+
+django_on_heroku.settings(locals())
